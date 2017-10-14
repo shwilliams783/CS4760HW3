1c9c111f19cbf984667a5038922c0e32cc2d9805
shwilliams783 <shwilliams783@gmail.com>
2017-10-14 16:16:19 -0500

    Shared Memory
    user.c now takes timeKey and msgKey arguments from the command line, and attaches pointers to shmTime and shmMsg in shared memory.
    
    Upon completion or SIGINT, user.c detaches from all shared memory

--- user.c
+++ user.c
@@ -9,11 +9,24 @@
 #include <sys/ipc.h>
 #include <sys/shm.h>
 
+struct timer
+{
+	int seconds;
+	int ns;
+};
+
+struct message
+{
+	pid_t pid;
+	int seconds;
+	int ns;
+};
+
 int errno;
 pid_t pid;
 char errmsg[100];
-int shmidTime;
-int shmidMsg;
+struct timer *shmTime;
+struct message *shmMsg;
 /* Insert other shmid values here */
 
 void sigIntHandler(int signum)
@@ -22,17 +35,10 @@ void sigIntHandler(int signum)
 	snprintf(errmsg, sizeof(errmsg), "USER %d: Caught SIGINT! Killing all child processes.", pid);
 	perror(errmsg);	
 	
-	/* errno = shmdt(shmTime);
+	errno = shmdt(shmTime);
 	if(errno == -1)
 	{
 		snprintf(errmsg, sizeof(errmsg), "MASTER: shmdt(shmTime)");
-		perror(errmsg);	
-	}
-	
-	errno = shmctl(shmidTime, IPC_RMID, NULL);
-	if(errno == -1)
-	{
-		snprintf(errmsg, sizeof(errmsg), "MASTER: shmctl(shmidTime)");
 		perror(errmsg);	
 	}
 
@@ -42,26 +48,62 @@ void sigIntHandler(int signum)
 		snprintf(errmsg, sizeof(errmsg), "MASTER: shmdt(shmMsg)");
 		perror(errmsg);	
 	}
-	
-	errno = shmctl(shmidMsg, IPC_RMID, NULL);
-	if(errno == -1)
-	{
-		snprintf(errmsg, sizeof(errmsg), "MASTER: shmctl(shmidMsg)");
-		perror(errmsg);	
-	}*/
 	exit(signum);
 }
 
 int main (int argc, char *argv[]) {
 int o;
 int i;
+int timeKey = atoi(argv[1]);
+int msgKey = atoi(argv[2]);
 key_t keyTime = 8675;
 key_t keyMsg = 1138;
 signal(SIGINT, sigIntHandler);
 pid = getpid();
 
-snprintf(errmsg, sizeof(errmsg), "USER %d: Slave process started!.", pid);
+snprintf(errmsg, sizeof(errmsg), "USER %d: Slave process started!", pid);
 perror(errmsg);
+
+/********************MEMORY ATTACHMENT********************/
+/* Point shmTime to shared memory */
+shmTime = shmat(timeKey, NULL, 0);
+if ((void *)shmTime == (void *)-1)
+{
+	snprintf(errmsg, sizeof(errmsg), "USER: shmat(shmidTime)");
+	perror(errmsg);
+    exit(1);
+}
+
+/* Point shmTime to shared memory */
+shmMsg = shmat(msgKey, NULL, 0);
+if ((void *)shmMsg == (void *)-1)
+{
+	snprintf(errmsg, sizeof(errmsg), "USER: shmat(shmidMsg)");
+	perror(errmsg);
+    exit(1);
+}
+/********************END ATTACHMENT********************/
+
+/* snprintf(errmsg, sizeof(errmsg), "USER %d: shmTime->seconds = %d shmTime->ns = %d\n", pid, shmTime->seconds, shmTime->ns);
+perror(errmsg);
+snprintf(errmsg, sizeof(errmsg), "USER %d: shmMsg->pid = %d shmMsg->seconds = %d shmMsg->ns = %d\n", pid, shmMsg->pid, shmMsg->seconds, shmMsg->ns);
+perror(errmsg); */
+
+/********************MEMORY DETACHMENT********************/
+errno = shmdt(shmTime);
+	if(errno == -1)
+	{
+		snprintf(errmsg, sizeof(errmsg), "MASTER: shmdt(shmTime)");
+		perror(errmsg);	
+	}
+
+	errno = shmdt(shmMsg);
+	if(errno == -1)
+	{
+		snprintf(errmsg, sizeof(errmsg), "MASTER: shmdt(shmMsg)");
+		perror(errmsg);	
+	}
+/********************END DETACHMENT********************/
 
 return 0;
 }
