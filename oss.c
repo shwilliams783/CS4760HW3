3a3ae7a7efb773619a911cae7d4f08137ea35e88
shwilliams783 <shwilliams783@gmail.com>
2017-10-14 16:08:39 -0500

    Shared Memory, Structs, and Timeout
    Added structs for timer and message
    timer struct contains seconds and ns
    message struct contains pid, seconds, and ns
    
    Implemented shared memory for shmTime and shmMsg
    Allocation, attachment, detatchment, and deallocation
    Also detatches and deallocates shared memory in the event of a SIGINT
    
    After spawning child processes, oss.c sends a kill signal to all child processes after maxTime, as determined by the -t option, then detatches and deallocates shared memory.

--- oss.c
+++ oss.c
@@ -9,44 +9,62 @@
 #include <sys/ipc.h>
 #include <sys/shm.h>
 
+struct timer
+{
+	int seconds;
+	int ns;
+};
+
+struct message
+{
+	pid_t pid;
+	int seconds;
+	int ns;
+};
+
 int errno;
 char errmsg[100];
 int shmidTime;
 int shmidMsg;
+struct timer *shmTime;
+struct message *shmMsg;
 /* Insert other shmid values here */
+
 
 void sigIntHandler(int signum)
 {
-	snprintf(errmsg, sizeof(errmsg), "MASTER: Caught SIGINT! Killing all child processes.");
+	/* Send a message to stderr */
+	snprintf(errmsg, sizeof(errmsg), "OSS: Caught SIGINT! Killing all child processes.");
 	perror(errmsg);	
 	
-	/* errno = shmdt(shmTime);
+	/* Deallocate shared memory */
+	errno = shmdt(shmTime);
 	if(errno == -1)
 	{
-		snprintf(errmsg, sizeof(errmsg), "MASTER: shmdt(shmTime)");
+		snprintf(errmsg, sizeof(errmsg), "OSS: shmdt(shmTime)");
 		perror(errmsg);	
 	}
 	
 	errno = shmctl(shmidTime, IPC_RMID, NULL);
 	if(errno == -1)
 	{
-		snprintf(errmsg, sizeof(errmsg), "MASTER: shmctl(shmidTime)");
+		snprintf(errmsg, sizeof(errmsg), "OSS: shmctl(shmidTime)");
 		perror(errmsg);	
 	}
 
 	errno = shmdt(shmMsg);
 	if(errno == -1)
 	{
-		snprintf(errmsg, sizeof(errmsg), "MASTER: shmdt(shmMsg)");
+		snprintf(errmsg, sizeof(errmsg), "OSS: shmdt(shmMsg)");
 		perror(errmsg);	
 	}
 	
 	errno = shmctl(shmidMsg, IPC_RMID, NULL);
 	if(errno == -1)
 	{
-		snprintf(errmsg, sizeof(errmsg), "MASTER: shmctl(shmidMsg)");
+		snprintf(errmsg, sizeof(errmsg), "OSS: shmctl(shmidMsg)");
 		perror(errmsg);	
-	}*/
+	}
 	exit(signum);
 }
 
@@ -55,6 +73,7 @@ int o;
 int i;
 int maxSlaves = 5;
 int numSlaves = 0;
+int numProc = 0;
 int maxTime = 20;
 char *sParam = NULL;
 char *lParam = NULL;
@@ -62,10 +81,12 @@ char *tParam = NULL;
 char timeArg[33];
 char msgArg[33];
 pid_t pid[20] = {getpid()};
+pid_t myPid;
 key_t keyTime = 8675;
 key_t keyMsg = 1138;
 FILE *fp;
 signal(SIGINT, sigIntHandler);
+
 
 
 /* Options */
@@ -111,45 +132,116 @@ if(tParam != NULL)
 {
 	maxTime = atoi(tParam);
 }
-printf("sParam = %d, tParam = %d\n", maxSlaves, maxTime);
+
+/********************MEMORY ALLOCATION********************/
+/* Create shared memory segment for a struct timer */
+shmidTime = shmget(keyTime, sizeof(struct timer), IPC_CREAT | 0666);
+if (shmidTime < 0)
+{
+	snprintf(errmsg, sizeof(errmsg), "OSS: shmget(keyTime...)");
+	perror(errmsg);
+	exit(1);
+}
+
+/* Point shmTime to shared memory */
+shmTime = shmat(shmidTime, NULL, 0);
+if ((void *)shmTime == (void *)-1)
+{
+	snprintf(errmsg, sizeof(errmsg), "OSS: shmat(shmidTime)");
+	perror(errmsg);
+    exit(1);
+}
+
+/* Create shared memory segment for a struct message */
+shmidMsg = shmget(keyMsg, sizeof(struct message), IPC_CREAT | 0666);
+if (shmidMsg < 0)
+{
+	snprintf(errmsg, sizeof(errmsg), "OSS: shmget(keyMsg...)");
+	perror(errmsg);
+	exit(1);
+}
+
+/* Point shmTime to shared memory */
+shmMsg = shmat(shmidMsg, NULL, 0);
+if ((void *)shmMsg == (void *)-1)
+{
+	snprintf(errmsg, sizeof(errmsg), "OSS: shmat(shmidMsg)");
+	perror(errmsg);
+    exit(1);
+}
+/********************END ALLOCATION********************/
+
+/* Convert shmTime and shmMsg keys into strings for EXEC parameters */
+sprintf(timeArg, "%d", shmidTime);
+sprintf(msgArg, "%d", shmidMsg);
+
+
+/* Set the time to 00.00 */
+shmTime->seconds = 0;
+shmTime->ns = 0;
+/* printf("shmTime->seconds = %d shmTime->ns = %d\n", shmTime->seconds, shmTime->ns); */
+
+/* Set the message to 0, 00.00 */
+shmMsg->pid = 0;
+shmMsg->seconds = 0;
+shmMsg->ns = 0;
+/* printf("shmMsg->pid = %d shmMsg->seconds = %d shmMsg->ns = %d\n", shmMsg->pid, shmMsg->seconds, shmMsg->ns); */
 
 /* Fork off child processes */
 for(i = 0; i < maxSlaves+1; i++)
 {
-	if(pid[i] != 0)
+	if(pid[i] != 0 && i < maxSlaves)
 	{
 		pid[i+1] = fork();
+		numSlaves++;
+		numProc++;
+		continue;
 	}
 	else if(pid[i] == 0)
 	{
-		/* snprintf(idArg, 10, "%d", index); */
-		/* snprintf(indexArg, 10, "%d", ((index-1)*5)); */
-		execl("./user", "user", (char*)0);
+		execl("./user", "user", timeArg, msgArg, (char*)0);
 	}
 }
-	
-return 0;
+
+sleep(maxTime);
+printf("OSS: Program allowed to complete!\n");
+
+/* Kill all slave processes */
+for(i = 1; i <= maxSlaves; i++)
+{
+	/* printf("Killing process #%d\n", pid[i]); */
+	kill(pid[i], SIGINT);
 }
 
+/********************DEALLOCATE MEMORY********************/
+errno = shmdt(shmTime);
+if(errno == -1)
+{
+	snprintf(errmsg, sizeof(errmsg), "OSS: shmdt(shmTime)");
+	perror(errmsg);	
+}
 
+errno = shmctl(shmidTime, IPC_RMID, NULL);
+if(errno == -1)
+{
+	snprintf(errmsg, sizeof(errmsg), "OSS: shmctl(shmidTime)");
+	perror(errmsg);	
+}
 
+errno = shmdt(shmMsg);
+if(errno == -1)
+{
+	snprintf(errmsg, sizeof(errmsg), "OSS: shmdt(shmMsg)");
+	perror(errmsg);	
+}
 
+errno = shmctl(shmidMsg, IPC_RMID, NULL);
+if(errno == -1)
+{
+	snprintf(errmsg, sizeof(errmsg), "OSS: shmctl(shmidMsg)");
+	perror(errmsg);	
+}
+/********************END DEALLOCATION********************/
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+return 0;
+}
